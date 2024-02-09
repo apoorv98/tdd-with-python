417a574d9a0aa8c8e60f8c74eab98eaeb85d45b0
Apoorv Jagtap <apoorvjagtap@yahoo.com>
2024-02-09 11:51:05 +0530

    sessions locally and on server

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import os
+import poplib
+import time
 from django.core import mail
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -59,3 +62,34 @@ class LoginTest(FunctionalTest):
         # navbar = self.browser.find_element(By.CSS_SELECTOR, '.navbar')
         # self.assertNotIn(TEST_EMAIL, navbar.text)
         self.wait_to_be_logged_out(email=TEST_EMAIL)
+
+    def wait_for_email(self, test_email, subject):
+        if not self.staging_server:
+            email = mail.outbox[0]
+            self.assertIn(test_email, email.to)
+            self.assertEqual(email.subject, subject)
+            return email.body
+
+        email_id = None
+        start = time.time()
+        inbox = poplib.POP3_SSL('pop.mail.yahoo.com')
+        try:
+            inbox.user(test_email)
+            inbox.pass_(os.environ['YAHOO_PASSWORD'])
+            while time.time() - start < 60:
+                # get 10 newest messages
+                count, _ = inbox.stat()
+                for i in reversed(range(max(1, count - 10), count + 1)):
+                    print('getting msg', i)
+                    _, lines, __ = inbox.retr(i)
+                    lines = [l.decode('utf8') for l in lines]
+                    print(lines)
+                    if f'Subject: {subject}' in lines:
+                        email_id = i
+                        body = '\n'.join(lines)
+                        return body
+                time.sleep(5)
+        finally:
+            if email_id:
+                inbox.dele(email_id)
+            inbox.quit()
--- superlist/settings.py
+++ superlist/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'lists',
     'accounts',
+    'functional_tests',
 ]
 
 AUTH_USER_MODEL = 'accounts.User'
@@ -136,3 +137,20 @@ EMAIL_HOST_USER = 'nidrorulma@gufum.com'
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
