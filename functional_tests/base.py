f8412c2f92d097255b83ba40c233f2e9e548ff82
Apoorv Jagtap <apoorvjagtap@yahoo.com>
2024-02-13 15:23:59 +0530

    new FT for sharing, move session creation to base

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -71,6 +71,20 @@ class FunctionalTest(StaticLiveServerTestCase):
             timestamp=timestamp
         )
 
+    def create_pre_authenticated_session(self, email):
+        if self.staging_server:
+            session_key = create_session_on_server(self.staging_server, email)
+        else:
+            session_key = create_pre_authenticated_session(email)
+        ## to set a cookie we need to first visit the domain
+        ## 404 pages load the quickest!
+        self.browser.get(self.live_server_url + "/404_no_such_url/")
+        self.browser.add_cookie(dict(
+            name=settings.SESSION_COOKIE_NAME,
+            value=session_key,
+            path='/',
+        ))
+
     @wait
     def wait_for(self, fn):
         start_time = time.time()
